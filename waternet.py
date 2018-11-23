d5d94a7898a0123ac8f40ef4260f731a78abe799
WizardZhT <956234606@qq.com>
2018-11-23 18:20:19 +0800

    change waternet

--- waternet.py
+++ waternet.py
@@ -16,18 +16,17 @@ class WaterNet(nn.Module):
     def __init__(self):
         super(WaterNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 5, kernel_size=1)
-        self.conv2 = nn.Conv2d(5, 10, kernel_size=3)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(470, 100)
-        self.fc2 = nn.Linear(100, 17)
+        self.fc1 = nn.Linear(1920, 1000)
+        self.fc2 = nn.Linear(1000, 200)
+        self.fc3 = nn.Linear(200, 17)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 470)
+        x = x.view(-1, 1920)
         x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
+        x = self.fc3(x)
         # print('=======after fc ======{}===='.format(x))
         return F.log_softmax(x, dim=1)
 
