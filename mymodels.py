08bdd7f0d9d676ebe5b29b2e67e0d8f50e487ce1
WizardZhT <956234606@qq.com>
2019-02-22 14:24:52 +0800

    a

--- mymodels.py
+++ mymodels.py
@@ -5,11 +5,11 @@ import torchvision.models as m
 
 class MyModels:
 
-	waternet = waternets.WaterNet()
-	waternetsf = waternets.WaterNetSmallFC()
-	waternetconvfc = waternets.WaterNetConvFC()
-	waterdsnet = waternets.WaterDenseNet()
-	waterdsnetf = waternets.WaterDenseNetFinal()
+    # waternet = waternets.WaterNet()
+    # waternetsf = waternets.WaterNetSmallFC()
+    # waternetconvfc = waternets.WaterNetConvFC()
+    # waterdsnet = waternets.WaterDenseNet()
+    waterdsnetf = waternets.WaterDenseNetFinal()
     waterdsnetf_in4_out58 = waternets.WaterDenseNet_in4_out58()
 
 
